c0d4d77c04e6bcaf8cf3b18b50caeb7e6714dc52
Ted Cowan <tcowan@my321.net>
2022-03-30 18:18:53 -0600

    first working

--- threads.c
+++ threads.c
@@ -2,51 +2,88 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <limits.h>
 
-void * threadFunction(void *arg) {
-	int number = *(int *)arg;
-	int count = 0;
-	while (engaged) {
-		count++;
-		usleep(20);
-	}
-	printf("thread %d count %d\n", number, count);
-	return NULL;
-}
+// Option-Shift-F to Reindent code in VSCode
 
 int engaged = 1;
 int sleepTime = 0;
-int seconds = 0;
+useconds_t microSeconds = 0;
 int threadCount = 0;
+void *threadFunction(void *arg)
+{
+	int number = *(int *)arg;
+	int count = 0;
+	while (engaged)
+	{
+		count++;
+		usleep(microSeconds);
+	}
+	printf("thread %d count %d\n", number, count);
+	*(int *)arg = count; // return count
+	return NULL;
+}
 
-int main(int argc, char ** argv) {
-	if (argc != 4) {
+int main(int argc, char **argv)
+{
+	if (argc != 4)
+	{
 		printf("Usage: ./threads THREADCOUNT SECONDS SLEEPTIME\n");
-		exit(1) 
+		exit(1);
 	}
 
 	threadCount = atoi(argv[1]);
-	seconds = atoi(argv[2]);
+	microSeconds = atoi(argv[2]);
 	sleepTime = atoi(argv[3]);
 
-	int indexes[count];
-	int threads[count];
+	pthread_t threads[threadCount];
 	int rc;
-
-	for (int i = 0; i < count; i++) {
-		rc = pthread_create(&threads[i], NULL, &threadFunction, &i);
-		if (rc) {
+	int index[threadCount]; // has thread number going in,
+							// count when thread ends
+	for (int i = 0; i < threadCount; i++)
+	{
+		index[i] = i;
+		rc = pthread_create(&threads[i], NULL, &threadFunction, &index[i]);
+		if (rc)
+		{
 			char msg[1024];
-			sprintf(msg, "Error creating thread %d", i);
+			sprintf(msg, "Error creating thread %d\n", i);
 			perror(msg);
 			exit(1);
 		}
-		sleep(sleepTime);
-		engaged = 0;
-		sleep(3);
-		int *p = &rc;
-		for(int i = 0; i < threadCount; i++) {
-			pthread_join(threads[i], &p);
-		}
 	}
+	sleep(sleepTime);
+	engaged = 0;
+	int maxCount = 0;
+	int minCount = INT_MAX;
+	int aveCount = 0;
+	float percent = 0.0;
+	for (int i = 0; i < threadCount; i++)
+	{
+		int threadRC = 0;
+		void *rcPtr = (void *)&threadRC;
+		pthread_join(threads[i], &rcPtr);
+	}
+	for (int i = 0; i < threadCount; i++)
+	{
+		if (index[i] > maxCount)
+		{
+			maxCount = index[i];
+		}
+		if (index[i] < minCount)
+		{
+			minCount = index[i];
+		}
+		aveCount += index[i]; // add count to bucket so we can average
+	}
+	aveCount = aveCount / threadCount; // calculate average count
+	percent = (maxCount - minCount) * 100.0 / aveCount;
+	printf("\n\nLowest Count:%d", minCount);
+	printf("\nHighest Count:%d", maxCount);
+	printf("\nAverage  Count:%d", aveCount);
+	printf("\nPercentage away from average:%.2f\n", percent);
+	// wRange = (maxCount - minCount) / aveCount * 100;
+	// printf("\nPercent Waiting Count Range:%.2f", wRange);
 
+	return 0;
+}
